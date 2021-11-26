51f1626d36ac8b6a9fed39d173c09198c3a360a3
Kieran-C <kieran.caruana@gmail.com>
2021-11-26 20:20:26 +0000

    Removed unused import

--- sqa-app/src/CipherLogic/DecryptVigenereCipher.js
+++ sqa-app/src/CipherLogic/DecryptVigenereCipher.js
@@ -4,8 +4,6 @@ Takes 2 args
 str Cipher text, str key
  */
 
-import VigenereCipher from "./VigenereCipher";
-
 export default function DecryptVigenereCipher(cipher_text, key) {
 
     let orig_text="";
