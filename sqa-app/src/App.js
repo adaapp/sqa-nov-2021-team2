c3803a9b7e7fe3956b37ab403d436153bf99b5de
Kieran-C <kieran.caruana@gmail.com>
2021-11-26 20:34:44 +0000

    Key goes to upper case in vigenere cipher

--- sqa-app/src/App.js
+++ sqa-app/src/App.js
@@ -75,7 +75,7 @@ function App() {
                 cipherTypeSelected={cipherType}
                 shiftValue={caesarCipherShift}
                 onShiftValueChange={handleCaesarShiftValueChange}
-                vigenereKey={userVigenereKey}
+                vigenereKey={userVigenereKey.toUpperCase()}
                 onVigenereKeyChange={handleVigenereKeyChange}
             />
             <h2>Pre Encryption</h2>
